ee80884f1dc916840e7b534549b9e13b2871d2db
Mikhail Potapov <devslashnil@gmail.com>
2022-02-21 18:13:36 +0300

    spring boot init

---
+++ src/main/java/com/devslashnil/adelie/AdelieApplication.java
@@ -0,0 +1,13 @@
+package com.devslashnil.adelie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdelieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdelieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devslashnil/adelie/AdelieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devslashnil.adelie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdelieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
